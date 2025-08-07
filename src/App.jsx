18a6c3de8b0ec3b8ad1974683cfe6a46f4b2c9d9
ronBP95 <ronmagpantay9513@gmail.com>
2025-08-06 21:35:06 -0700

    Add react icons, build out base for chatbot, set css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css'
 
+// Icon Imports
+import { HiUserCircle, HiDotsVertical } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
+
 function App() {
   return (
     <>
       <div className="parentContainer">
-        <div className="userName">
-          <div className="chatBox">
-            Chat Content
-          </div>
-        </div>
+            <div className="userName">
+              <div className="userIcons">
+                <div><HiUserCircle size ="2em"/></div>
+                <div className="userText">Support Agent</div>
+              </div>
+                <div className="userButtons">
+                  <HiDotsVertical size="1.5em"/>
+                  <IoMdClose size="1.5em"/>
+                </div>
+            </div>
+            
+            <div className="chatBox">
+              Chat Content
+            </div>
+            <div className="chatInput">
+              Chat Input
+            </div>
       </div>
     </>
   )
